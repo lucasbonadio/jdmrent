819fbae1f7c68b821a9cf6d1b850af51eff9dea6
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-10-24 05:55:11 -0300

    chore: folder pattern and tsconfig styled components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+import {
+    Container, Title
+} from './styles'
+
+const Home = () => {
+    return (
+        <Container>
+            <Title>HOME</Title>
+        </Container>
+    )
+}
+
+export default Home;
